ce85f79f9b88dc936b7ee3f9b4202d2e0400a9cb
Shelamov Ruslan <shelamov.ruslan@gmail.com>
2022-06-09 01:02:31 +0300

    3

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -3,25 +3,45 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientHandler implements Runnable{
 
     private final Socket socket;
+    private String name;
+    TestDrive testDrive;
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public ClientHandler(Socket socket) {
+    public String getName() {
+        return name;
+    }
+
+    public TestDrive getTestDrive() {
+        return testDrive;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public ClientHandler(Socket socket, TestDrive testDrive) {
         this.socket = socket;
+        this.testDrive = testDrive;
     }
 @Override
     public void run () {
         String line;
 
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                 PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
+                PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
                 writer.println("Hello, what is your name?");
+                setName(reader.readLine());
                 do {
                     line = reader.readLine();
-                    writer.println(line + " hello");
-                } while (!line.isBlank());
+                   this.testDrive.sendMassage(line, getName());
+                } while (true);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             } finally {
@@ -33,4 +53,6 @@ public class ClientHandler implements Runnable{
             }
 
     }
+
+
 }
--- src/main/java/TestDrive.java
+++ src/main/java/TestDrive.java
@@ -5,30 +5,40 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TestDrive {
 
 
-
+    private ArrayList<ClientHandler> clients = new ArrayList<>();
 
     public static void main(String[] args) {
         new TestDrive().run();
     }
     private void run () {
 
-
         try (ServerSocket serverSocket = new ServerSocket(9988)) {
             while (true) {
                 Socket socket = serverSocket.accept();
-                ClientHandler clientHandler = new ClientHandler(socket);
-
+                ClientHandler clientHandler = new ClientHandler(socket, this);
+                this.clients.add(clientHandler);
                 new Thread(clientHandler).start();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
-
-
+    public synchronized void sendMassage (String massage, String name){
+        ArrayList<ClientHandler> clientsTempList = this.clients;
+        for (ClientHandler clientHandler: clientsTempList){
+            Socket socket = clientHandler.getSocket();
+            try {
+                PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+                writer.printf("%s пишет: %s\n", name, massage);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }

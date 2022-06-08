898ee4efbdcad43b430905ffb3529bcf9d971e4a
Shelamov Ruslan <shelamov.ruslan@gmail.com>
2022-06-08 20:30:14 +0300

    1

---
+++ src/main/java/ClientHandler.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable{
+
+    private final Socket socket;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+@Override
+    public void run () {
+        String line;
+        do {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
+                writer.println("Hello, what is your name?");
+                do {
+                    line = reader.readLine();
+                    writer.println(line + " hello");
+                } while (!line.isBlank());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } while (!line.isBlank());
+    }
+}
---
+++ src/main/java/TestDrive.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class TestDrive {
+
+    static ArrayList<ClientHandler> clients = new ArrayList<>();
+
+    public static void main(String[] args) {
+        new TestDrive().run();
+    }
+    private void run () {
+
+
+        try (ServerSocket serverSocket = new ServerSocket(9988)) {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(socket);
+                clients.add(clientHandler);
+                new Thread(clientHandler).start();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+    }
+}
